1d934e83a2a4b6ada284593598458a8214b97b3a
ericbrambilaifc <ericbrambilaifc@gmail.com>
2025-06-23 16:18:16 -0300

    conexao

---
+++ ConexaoBD.php
@@ -0,0 +1,5 @@
+<?php
+
+    class ConexaoBD{   
+        public static function conectar():PDO{$conexao = new PDO("pgsql:host=localhost; dbname=terceiro_a","postgres","postgres"); return $conexao;}
+    }
